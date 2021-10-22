87d706bd0f4403aa12d5dcea0a5158a279e2903f
Sahil Behl <sahilbehl4@gmail.com>
2021-10-22 16:27:28 -0300

    feat: created a logo view and navigation bar

--- Touchdown/App/ContentView.swift
+++ Touchdown/App/ContentView.swift
@@ -9,8 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        FooterView()
-            .padding()
+        ZStack {
+            VStack(spacing: 0) {
+                NavigationBarView()
+                    .padding(.horizontal, 15)
+                    .padding(.bottom)
+                    .padding(.top, UIApplication.shared.windows.first?.safeAreaInsets.top)
+                    .background(Color.white)
+                    .shadow(color: Color.black.opacity(0.05), radius: 5, x: 0, y: 5)
+                
+                Spacer()
+                
+                FooterView()
+                    .padding(.horizontal)
+            }
+            .background(colorBackground.ignoresSafeArea())
+        }
+        .ignoresSafeArea(.all, edges: .top)
     }
 }
 
---
+++ Touchdown/View/Home/LogoView.swift
@@ -0,0 +1,37 @@
+//
+//  LogoView.swift
+//  Touchdown
+//
+//  Created by Sahil Behl on 2021-10-22.
+//
+
+import SwiftUI
+
+struct LogoView: View {
+    var body: some View {
+        HStack {
+            Text("Touch".uppercased())
+                .font(.title3)
+                .fontWeight(.black)
+                .foregroundColor(.black)
+            
+            Image("logo-dark")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 30, height: 40, alignment: .center)
+            
+            Text("Down".uppercased())
+                .font(.title3)
+                .fontWeight(.black)
+                .foregroundColor(.black)
+        }
+    }
+}
+
+struct LogoView_Previews: PreviewProvider {
+    static var previews: some View {
+        LogoView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
